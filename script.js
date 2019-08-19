e8e257e84455b8e3b02b2c8f3a56f1cff77a1a7c
millerwarren <53824450+millerwarren@users.noreply.github.com>
2019-08-19 10:29:08 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// var h1 = document.querySelector('h1');
+
+// h1.style.color = 'green';
+
+// var body = document.querySelector('body');
+
+// var isOrange = false;
+
+// setInterval(function () {
+//     if (isOrange) {
+//         body.style.backgroundColor = 'white';
+//     } else {
+//         body.style.backgroundColor = 'orange'
+//     }
+
+//     isOrange = !isOrange;
+// }, 3000);
+
+var tag = document.querySelector("p");
+
+var tag = document.getElementById("first");
+
+var tag = document.getElementsByClassName("special");
+
+var tag = document.getElementsByTagName("p");
